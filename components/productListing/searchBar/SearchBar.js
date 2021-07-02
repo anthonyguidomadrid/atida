61b0c33ff11a99609e5c55c004f0ec6832f34ad5
Anthony Guido <guido.anthony@orange.fr>
2021-07-02 14:00:02 +0200

    change search bar

--- components/productListing/searchBar/SearchBar.js
+++ components/productListing/searchBar/SearchBar.js
@@ -6,15 +6,6 @@ const SearchBar = ({filterProductsBar}) => {
 
     const [query, setQuery] = useState()
 
-    const setKeyword = e => {
-        setQuery(e.target.value)
-        sendQuery(query)
-    }
-
-    const sendQuery = keyword => {
-        filterProductsBar(keyword)
-    }
-
     return (
         <div className={styles.searchBar}>
             <FormControl type="text" value={query} onChange={e => filterProductsBar(e.target.value)} placeholder="Search" className="mr-sm-2" />
